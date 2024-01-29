7dd0d0ccaeb36ae056d16ed2a0fadda4428b9c9b
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-01-29 09:14:32 +0800

    Update Order controller and route

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,67 +4,82 @@ const User = require("../models/User");
 
 // Checkout order
 module.exports.checkout = async (req, res) => {
-    try {
-        const { id } = req.user;
-        // Get user cart
-        const userCart = await Cart.findOne({ userId: id });
+  try {
+    const { id } = req.user;
+    // Get user cart
+    const userCart = await Cart.findOne({ userId: id });
 
-
-        if (!userCart) {
-            return res.status(400).json({ error: 'User cart not found' });
-        }
-
-        // Creates new Order
-        const newOrder = new Order({
-            userId: userCart.userId,
-            productsOrdered: userCart.cartItems,
-            totalPrice: userCart.totalPrice,
-        });
-
-        // Saves the order to the database
-        await newOrder.save();
-
-        // // Clears all items inside the cart
-        const clearCart = await Cart.findOneAndUpdate(
-            { userId: id },
-            { cartItems: [], totalPrice: 0 },
-            { new: true }
-        );
-
-        // return res.status(201).json(true);
-        return res.status(201).json({ order: newOrder, cart: clearCart });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (!userCart) {
+      return res.status(400).json({ error: "User cart not found" });
     }
+
+    // Creates new Order
+    const newOrder = new Order({
+      userId: userCart.userId,
+      productsOrdered: userCart.cartItems,
+      totalPrice: userCart.totalPrice,
+    });
+
+    // Saves the order to the database
+    await newOrder.save();
+
+    // // Clears all items inside the cart
+    const clearCart = await Cart.findOneAndUpdate(
+      { userId: id },
+      { cartItems: [], totalPrice: 0 },
+      { new: true }
+    );
+
+    // return res.status(201).json(true);
+    return res.status(201).json({ order: newOrder, cart: clearCart });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Retrieve logged in user's orders
 module.exports.getUserOrders = async (req, res) => {
-    try {
-        const { id } = req.user;
-        const userOrders = await Order.find({ userId: id });
+  try {
+    const { id } = req.user;
+    const userOrders = await Order.find({ userId: id });
 
-        if (!userOrders) {
-            return res.status(404).json({ error: "No orders found." });
-        }
-
-        return res.status(200).json({ message: "Here are your orders", data: userOrders });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (!userOrders) {
+      return res.status(404).json({ error: "No orders found." });
     }
+
+    return res.status(200).json({ message: "Here are your orders", data: userOrders });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Retrieve all orders
 module.exports.getAllOrders = async (req, res) => {
-    try {
-        // Gets all orders
-        const allOrders = await Order.find();
+  try {
+    // Gets all orders
+    const allOrders = await Order.find();
 
-        return res.status(200).json({ message: "Showing all orders", data: allOrders });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    return res.status(200).json({ message: "Showing all orders", data: allOrders });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Update order status
+module.exports.updateStatus = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    // Updates order status
+    const order = await Order.findByIdAndUpdate(orderId, { status }, { new: true });
+
+    return res.status(200).json({ message: `Status has been updated to ${status}`, data: order });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -16,5 +16,8 @@ router.get("/my-orders", verify, orderController.getUserOrders);
 // Retrieve all orders (admin only)
 router.get("/all-orders", verify, verifyAdmin, orderController.getAllOrders);
 
+// Update order status
+router.patch("/:orderId/update-order-status", verify, verifyAdmin, orderController.updateStatus);
+
 // [SECTION] Export Route System
 module.exports = router;
